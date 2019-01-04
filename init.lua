e4438b6611a3123f0aa68827760745561e5fa7e3
dasheng <kaichaosuna@gmail.com>
2019-01-05 00:40:57 +0800

    add scroll left and right

--- init.lua
+++ init.lua
@@ -1,12 +1,25 @@
+-- Test command
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
   hs.alert.show("Hello World!")
 end)
 
+-- Move up
 hs.hotkey.bind({"ctrl"}, "K", function()
-  hs.eventtap.scrollWheel({0, 3}, {})
+  hs.eventtap.scrollWheel({0, 2}, {})
 end)
 
+-- Move down
 hs.hotkey.bind({"ctrl"}, "J", function()
-  hs.eventtap.scrollWheel({0, -3}, {})
+  hs.eventtap.scrollWheel({0, -2}, {})
+end)
+
+-- Move left
+hs.hotkey.bind({"ctrl"}, "H", function()
+  hs.eventtap.scrollWheel({2, 0}, {})
+end)
+
+-- Move right
+hs.hotkey.bind({"ctrl"}, "L", function()
+  hs.eventtap.scrollWheel({-2, 0}, {})
 end)
 
