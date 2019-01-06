e98ff86439fe3f7a057727184efee17a8ccbe9db
dasheng <kaichaosuna@gmail.com>
2019-01-06 18:25:57 +0800

    refactor with global variables.

--- init.lua
+++ init.lua
@@ -1,25 +1,28 @@
+primaryKey = "ctrl"
+verticalMoveInterval = 2
+horizontalMoveInterval = 2
+
 -- Test command
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
   hs.alert.show("Hello World!")
 end)
 
 -- Move up
-hs.hotkey.bind({"ctrl"}, "K", function()
-  hs.eventtap.scrollWheel({0, 2}, {})
+hs.hotkey.bind({primaryKey}, "K", function()
+  hs.eventtap.scrollWheel({0, verticalMoveInterval}, {})
 end)
 
 -- Move down
-hs.hotkey.bind({"ctrl"}, "J", function()
-  hs.eventtap.scrollWheel({0, -2}, {})
+hs.hotkey.bind({primaryKey}, "J", function()
+  hs.eventtap.scrollWheel({0, -verticalMoveInterval}, {})
 end)
 
 -- Move left
-hs.hotkey.bind({"ctrl"}, "H", function()
-  hs.eventtap.scrollWheel({2, 0}, {})
+hs.hotkey.bind({primaryKey}, "H", function()
+  hs.eventtap.scrollWheel({horizontalMoveInterval, 0}, {})
 end)
 
 -- Move right
-hs.hotkey.bind({"ctrl"}, "L", function()
-  hs.eventtap.scrollWheel({-2, 0}, {})
+hs.hotkey.bind({primaryKey}, "L", function()
+  hs.eventtap.scrollWheel({-horizontalMoveInterval, 0}, {})
 end)
-
