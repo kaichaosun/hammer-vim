e0887f240ef46df335eec476984799cc08565661
dasheng <kaichaosuna@gmail.com>
2019-01-06 21:26:28 +0800

    add continuous command.

--- init.lua
+++ init.lua
@@ -1,28 +1,51 @@
 primaryKey = "ctrl"
+secondaryKey = "shift"
 verticalMoveInterval = 2
-horizontalMoveInterval = 2
+horizontalMoveInterval = 1
 
--- Test command
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
-  hs.alert.show("Hello World!")
-end)
-
+-- --------------Vim like command--------------
 -- Move up
-hs.hotkey.bind({primaryKey}, "K", function()
-  hs.eventtap.scrollWheel({0, verticalMoveInterval}, {})
-end)
+hs.hotkey.bind({ primaryKey }, "K",
+  function()
+    hs.eventtap.scrollWheel({ 0, verticalMoveInterval }, {})
+  end,
+  nil,
+  function()
+    hs.eventtap.scrollWheel({ 0, verticalMoveInterval }, {})
+  end)
 
 -- Move down
-hs.hotkey.bind({primaryKey}, "J", function()
-  hs.eventtap.scrollWheel({0, -verticalMoveInterval}, {})
-end)
+hs.hotkey.bind({ primaryKey }, "J",
+  function()
+    hs.eventtap.scrollWheel({ 0, -verticalMoveInterval }, {})
+  end,
+  nil,
+  function()
+    hs.eventtap.scrollWheel({ 0, -verticalMoveInterval }, {})
+  end)
 
 -- Move left
-hs.hotkey.bind({primaryKey}, "H", function()
-  hs.eventtap.scrollWheel({horizontalMoveInterval, 0}, {})
-end)
+hs.hotkey.bind({ primaryKey }, "H",
+  function()
+    hs.eventtap.scrollWheel({ horizontalMoveInterval, 0 }, {})
+  end,
+  nil,
+  function()
+    hs.eventtap.scrollWheel({ horizontalMoveInterval, 0 }, {})
+  end)
 
 -- Move right
-hs.hotkey.bind({primaryKey}, "L", function()
-  hs.eventtap.scrollWheel({-horizontalMoveInterval, 0}, {})
+hs.hotkey.bind({ primaryKey }, "L",
+  function()
+    hs.eventtap.scrollWheel({ -horizontalMoveInterval, 0 }, {})
+  end,
+  nil,
+  function()
+    hs.eventtap.scrollWheel({ -horizontalMoveInterval, 0 }, {})
+  end)
+
+-- --------------Utility command--------------
+-- Show date time
+hs.hotkey.bind({ primaryKey, secondaryKey }, "T", function()
+  hs.alert.show(os.date("%c"))
 end)
